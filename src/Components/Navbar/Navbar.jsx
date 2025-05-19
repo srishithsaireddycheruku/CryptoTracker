35b62faa87ea289012a3570a7209de32b2508a22
Srishith Sai Reddy Cheruku <srishithsaireddycheruku@gmail.com>
2025-05-19 15:35:23 +0530

    made the home buttom to direct to home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 // import { useContext } from "react";
 // import { CurrencyContext } from "../../CurrencyContext/CurrencyContext";
 import store from "../../zustandStore/zustandStore.jsx";
+import { useNavigate } from "react-router-dom";
 function Navbar(){
 
     // const {setCurrency} = useContext(CurrencyContext);
 
     const {setCurrency} = store();
-
+    const navigate = useNavigate();
+    function goToHome(){
+        navigate("/");
+    }
     return (
         <>
             <div className="navbar bg-base-100">
@@ -34,7 +38,7 @@ function Navbar(){
                         </ul>
                     </div>
                 </div>
-                <div className="navbar-center">
+                <div onClick={goToHome} className="navbar-center">
                     <a className="btn btn-ghost text-xl">daisyUI</a>
                 </div>
                 <div className="navbar-end">
