9c4f270b89a5b5b436c39c571e4888900d9e69a4
Srishith Sai Reddy Cheruku <srishithsaireddycheruku@gmail.com>
2026-03-04 23:45:47 +0530

    added the navbar and the banner of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-
+import CoinTable from "./Components/CoinTable/CoinTable";
+import { useEffect } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Banner from "./Components/Banner/Banner";
 function App() {
-
 
   return (
     <>
-      Hello
+      <Navbar/>
+      <Banner/>
+      <CoinTable/>
     </>
   )
 }
---
+++ src/Components/CoinTable/CoinTable.jsx
@@ -0,0 +1,13 @@
+import { useEffect,useState } from "react";
+
+
+function CoinTable(){
+
+    return(
+        <>
+            CoinTable
+        </>
+    )
+}
+
+export default CoinTable;
